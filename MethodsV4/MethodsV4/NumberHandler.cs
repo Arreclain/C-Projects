b199e7002bec6a59fc5509ec7381973a9b55d3a3
Arreclain <arreclain@gmail.com>
2025-08-21 10:54:14 -0700

    First attempt at this class.

---
+++ MethodsV4/MethodsV4/NumberHandler.cs
@@ -0,0 +1,15 @@
+namespace MethodsV4;
+
+public partial class Program
+{
+    public class NumberHandler
+    {
+        public void NumberCrunch(int first, int second)
+        {
+            //The rubric requests I do math with the first input, but it's a void method so...
+            first = first + 2;
+            Console.WriteLine("I presume you want me to display the second number ("+second+") to the console?");
+            
+        }
+    }
+}
---
+++ MethodsV4/MethodsV4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodsV4
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            //I have instantiated the class.
+            NumberHandler hello = new NumberHandler();
+            
+            
+            //I have called the method passing in two numbers.
+            //I think I have named each entry?
+            hello.NumberCrunch(first: 4, second: 5);
+        }
+    }
+}
+
+
