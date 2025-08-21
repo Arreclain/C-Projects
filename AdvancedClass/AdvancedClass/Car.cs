f0e2e310cc79b99e4b9a6f0dba0c63d5d04660cf
Arreclain <arreclain@gmail.com>
2025-08-21 13:28:51 -0700

    Trying to get this to work.

--- AdvancedClass/AdvancedClass/Car.cs
+++ AdvancedClass/AdvancedClass/Car.cs
@@ -2,21 +2,13 @@ namespace AdvancedClass{
 
     class Car
     {
-        public Car()
-        {
-        int gas = 12;
-        int tickets = 0;
+
+        public int Gas { get; set; }
+
+        private int Tickets { get; set; }
         }
 
-        public int getGas(Car car)
-        {
-            int currentGas = car.gas;
-            return currentGas;
-        }
 
-        public void Drive()
-        {
-            Console.WriteLine("Zoom!");
-        }
+
+         public void Drive() {Console.WriteLine("Zoom!"); }
     }
-}
