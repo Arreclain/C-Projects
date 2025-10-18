d28a9a0bebd103ffce07204f65177b179b47177e
Arreclain <arreclain@gmail.com>
2025-10-18 15:40:45 -0700

    Updated the NumberHandler class to print out the first number.

--- MethodsV4/MethodsV4/NumberHandler.cs
+++ MethodsV4/MethodsV4/NumberHandler.cs
@@ -6,8 +6,15 @@ public partial class Program
     {
         public void NumberCrunch(int first, int second)
         {
+            //The first input printed out.
+            Console.WriteLine("The first number equals: " + first);
+            
             //The rubric requests I do math with the first input, but it's a void method so...
             first = first + 2;
+            
+            //As per the instructor, here is the output of the operation:
+            Console.WriteLine("First number post math: " + first);
+            
             Console.WriteLine("I presume you want me to display the second number ("+second+") to the console?");
             
         }
