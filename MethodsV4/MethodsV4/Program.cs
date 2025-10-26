f438af752ee1fe4e6d49ee4de6dd2b7c43104517
Arreclain <arreclain@gmail.com>
2025-10-26 13:22:15 -0700

    Added code for an instructor.

--- MethodsV4/MethodsV4/Program.cs
+++ MethodsV4/MethodsV4/Program.cs
@@ -13,7 +13,18 @@ namespace MethodsV4
             //I have called the method passing in two numbers.
             //I think I have named each entry?
             hello.NumberCrunch(first: 4, second: 5);
+            
+            System.Console.WriteLine("Hello!  An instructor would like this to run with user provided numbers." +
+                                     "Please provide a non-zero digit.");
+            int number1 = Convert.ToInt16(Console.ReadLine());
+            System.Console.WriteLine("Hello!  I now require a second number." +
+                                     "Please provide a non-zero digit.");
+            int number2 = Convert.ToInt16(Console.ReadLine());
+            hello.NumberCrunch(first: number1, second: number2);
+            
         }
+        
+        
     }
 }
 
